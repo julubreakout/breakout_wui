6f8e4df625496702bb9fbbdb419966c014b48843
lukaseckert <lukas.eckert@htwg-konstanz.de>
2013-11-21 13:03:57 +0100

    added level select qand jsonsd

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,7 +1,12 @@
 package controllers;
 
+import java.io.File;
+
 import play.mvc.Controller;
 import play.mvc.Result;
+
+import com.google.gson.Gson;
+
 import de.luma.breakout.communication.ObservableGame.GAME_STATE;
 import de.luma.breakout.communication.ObservableGame.MENU_ITEM;
 import de.luma.breakout.controller.IGameController.PLAYER_INPUT;
@@ -13,9 +18,7 @@ public class Application extends Controller  {
 	public static StaticGameInstance getGame() {
 		if (gameInstance == null) {
 			gameInstance = new StaticGameInstance();
-			
-		}
-		
+		}		
 		return gameInstance;
 	}
 		
@@ -32,11 +35,32 @@ public class Application extends Controller  {
      */
     public static Result playGrid() {
     	if (getGame().gameController.getState() == GAME_STATE.RUNNING) {   // render playgrid
-    		return ok(views.html.gamegrid.render(getGame().getBricks(), getGame().getBalls()));
+    		return ok(views.html.gamegrid.render(
+    				getGame().gameController.getGridSize().width,
+    				getGame().gameController.getGridSize().height,
+    				getGame().getBricks(), getGame().getBalls()));
+    	} else if (getGame().gameController.getState() == GAME_STATE.MENU_LEVEL_SEL) {
+    		return getLevels();
     	} else {  // render menu
     		return ok(views.html.menu.render(getGame().getMenuItems()));
     	}
     }
+    
+    
+    private static Result getLevels() {   	    	
+    	Gson gson = new Gson();
+    	String json = gson.toJson(getGame().gameController.getLevelList());
+
+    	response().setContentType("Application.levellist");
+ 		return ok(json); 
+    }
+    
+    public static Result loadLevel(String file) {
+    	getGame().gameController.loadLevel(new File(file));
+    	getGame().gameController.processMenuInput(MENU_ITEM.MNU_CONTINUE);
+    	return ok();
+    }
+    
     
     /**
      * Processes a click on a menu button
@@ -58,8 +82,10 @@ public class Application extends Controller  {
 			break;
 		case "right":
 			getGame().gameController.processGameInput(PLAYER_INPUT.RIGHT);
+			getGame().gameController.processGameInput(PLAYER_INPUT.RIGHT);
 			break;
 		case "left":
+			getGame().gameController.processGameInput(PLAYER_INPUT.LEFT);
 			getGame().gameController.processGameInput(PLAYER_INPUT.LEFT);
 			break;
 		default:
